75bccd7dca3959362535fd1c18a8a4b872ab02cc
erhic <eric.gichovi@student.moringaschool.com>
2022-01-16 10:18:44 +0300

    importing my custom classes in service,for instanciation

--- GitSearch/src/app/profile.service.ts
+++ GitSearch/src/app/profile.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Users } from './users';
+import { Repository } from './repository';
 
 @Injectable({
   providedIn: 'root'
