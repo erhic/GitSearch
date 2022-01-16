d4d25f3a25bf5b59aa64d05db658bf3b50a4ad43
erhic <eric.gichovi@student.moringaschool.com>
2022-01-16 07:32:49 +0300

    generating components

--- GitSearch/src/app/app.module.ts
+++ GitSearch/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    RepoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
