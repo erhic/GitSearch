a1c680dbe5bb5a011b745ead8543488e1e17de06
erhic <eric.gichovi@student.moringaschool.com>
2022-01-16 12:31:47 +0300

    setting the api key

--- GitSearch/src/environments/environment.ts
+++ GitSearch/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'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',
+  apiUrl:'https://api.github.com/users',
 };
 
 /*
