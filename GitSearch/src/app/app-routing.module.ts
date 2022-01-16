53254f84bc93cb9b20bdff55b94529be133bcc17
erhic <eric.gichovi@student.moringaschool.com>
2022-01-16 07:39:48 +0300

    set up routing of  the navbar links

--- GitSearch/src/app/app-routing.module.ts
+++ GitSearch/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  
+import { HomeComponent } from './home/home.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [ {
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'home'
+},
+  {path:'home',component:HomeComponent},
+  {path:'repo',component:RepoComponent},
+  {path:'user',component:UserComponent},
 ];
 
 @NgModule({
